509f30be348fb801cb3d7f1f6545f1132ae26a31
Fábio Lourenço <faboyds@gmail.com>
2018-04-14 00:00:24 +0100

    New path for Trello login

--- todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
+++ todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
@@ -2,11 +2,12 @@ package com.casarder.todopick.services
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * Created by fabiolourenco on 13/04/18.
  */
 interface TrelloService {
-    @GET("1/authorize/")
-    fun login() : Call<String>
+    @GET("1/authorize/https://trello.com/1/authorize?expiration=30days&name=SnapIt&scope=read,write,account&response_type=token&key={key}")
+    fun login(@Path("key") key : String) : Call<String>
 }
