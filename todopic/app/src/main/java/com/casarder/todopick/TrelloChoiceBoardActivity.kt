efd3095006144cdc91e4daf0eb25abc419a8afeb
Catarina Fernandes <catarina.fernandes@blip.pt>
2018-04-13 23:48:39 +0100

    SCR: NA activitys created

---
+++ todopic/app/src/main/java/com/casarder/todopick/TrelloChoiceBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.casarder.todopick
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TrelloChoiceBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trello_choice_board)
+    }
+}
