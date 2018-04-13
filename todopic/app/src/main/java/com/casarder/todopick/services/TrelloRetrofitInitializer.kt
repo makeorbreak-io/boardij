d9ec7acf62c97168461d434a7ee697cf442c6d5f
Fábio Lourenço <faboyds@gmail.com>
2018-04-13 23:34:38 +0100

    Adding dependencies

--- todopic/app/src/main/java/com/casarder/todopick/services/TrelloRetrofitInitializer.kt
+++ todopic/app/src/main/java/com/casarder/todopick/services/TrelloRetrofitInitializer.kt
@@ -1,10 +1,17 @@
 package com.casarder.todopick.services
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * Created by fabiolourenco on 13/04/18.
  */
-class RetrofitInitializer {
-    fun init() {
-        Retrofit.Builder().baseUrl("http://192.168.0.23:8080/")
-    }
+class TrelloRetrofitInitializer {
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.trello.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+    fun trelloService () : TrelloService = retrofit.create(TrelloService::class.java)
 }
--- todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
+++ todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
@@ -1,5 +1,12 @@
 package com.casarder.todopick.services
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 /**
  * Created by fabiolourenco on 13/04/18.
  */
+interface TrelloService {
+    @GET("1/authorize/")
+    fun login() : Call<String>
+}
