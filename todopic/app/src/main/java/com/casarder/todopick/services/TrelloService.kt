58832e1af7f169ad1ee8b40c979d266b460a31c9
Fábio Lourenço <faboyds@gmail.com>
2018-04-14 00:57:30 +0100

    LEL NAO SEI O QUE ESTOU A FAZER

--- todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
+++ todopic/app/src/main/java/com/casarder/todopick/services/TrelloService.kt
@@ -8,6 +8,17 @@ import retrofit2.http.Path
  * Created by fabiolourenco on 13/04/18.
  */
 interface TrelloService {
-    @GET("1/authorize/https://trello.com/1/authorize?expiration=30days&name=SnapIt&scope=read,write,account&response_type=token&key={key}")
+
+    //user token e4b2cf28c60a1cdc4af322757b5aeb71
+
+    /*
+    {
+        "name": "CARD NAME",
+        "des": "Card description",
+        "pos": "top",
+    }
+     */
+
+    @GET("")
     fun login(@Path("key") key : String) : Call<String>
 }
